6505ce299c58938849037181fbd2c8fb8d89f93f
Daniel <60454828+isleepontree10@users.noreply.github.com>
2022-03-18 23:31:54 -0400

    Add files via upload

--- PPscheduler.py
+++ PPscheduler.py
@@ -2,13 +2,15 @@ import time
 from myscheduler import Priority
 import readfile
 import threading
+from time import sleep
+from collections import deque
 
 #prearrivalQ=[].... this would hole the processes before they arrived 
 
 queue1=[]
 queue2=[]
 
-class scheduler:
+class scheduler(threading.Thread):
     
     def timeSlot(self, priority):
         prio = priority       
@@ -24,14 +26,61 @@ class scheduler:
 
 
 
-    def runTimeAllocation():
+    def run():
 
+        queue1=True
 
         while True:
-                        
+
+            if :
+                #determines the false/true q and sorts elements depending on their priority 
+
+
+            elif:
+                #if there is an element that arrives puts in the false q (expired q)  
+                #
+
+
+            elif queue1 != 0 and  queue1 == True:
+                #queue1 manipulate element [0] to do a timeslice 
+                # if hte element is paused  then resume
+                #queue1.pop[0] 
+                
+                #do timeslice and pauses PID
+                
+                
+                if queue1.empty():
+                    queue1=False
+                #empty make it false and q 2 true 
+
+                
+                
+                #print "Time" + clock + PID + status + "Granted" + time slice      ... or print func idc
+                
+                sleep(1)
+
+
+
+
+            elif queue2 !=0 and queue2 == True:
+                queue2.popleft()   
+                
+                
+                sleep(1)
+
             
-            if activeQ==0:
-                activeQ,expiredQ = expiredQ , activeQ
+
+            else:
+                break
+
+
+            
+        
+
+
+
+
+
 
 
 
