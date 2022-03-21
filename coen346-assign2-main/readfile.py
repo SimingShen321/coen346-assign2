ebc2537acde1cd4b861c780079b074fa4cc1af2a
Daniel <60454828+isleepontree10@users.noreply.github.com>
2022-03-20 23:21:23 -0400

    push :)

---
+++ coen346-assign2-main/readfile.py
@@ -0,0 +1,73 @@
+from array import *
+import functools
+import operator
+from re import I
+def getlines():
+    with open("input.txt", "r") as file:
+        line = []    
+        for i in file:
+            stripped_line = i.strip()
+            if not i.startswith("#"):
+                line.append(i.strip())
+
+        return line
+            
+def getwords():          
+    word = []        
+    for i in getlines():
+        strippedword = i.split()
+        word.append(i.split())
+    return word
+
+def get_num_of_processes():
+    num = getwords()
+    return num[0][0]
+
+
+def getID():
+    flatten = [item for sublist in getwords() for item in sublist]  
+    ids = []
+    for i in range(1, len(flatten), 4):
+        ids.append(flatten[i])  
+    return ids
+    
+def getArrivaltime():
+    flatten = [item for sublist in getwords() for item in sublist]  
+    arrival = []
+    for i in range(2, len(flatten), 4):
+        arrival.append(flatten[i])  
+    return arrival
+
+def getBursttime():
+    flatten = [item for sublist in getwords() for item in sublist]  
+    burst = []
+    for i in range(3, len(flatten), 4):
+        burst.append(flatten[i]) 
+    return burst
+        
+def getPriority():
+    flatten = [item for sublist in getwords() for item in sublist]  
+    initialPriority = []
+    for i in range(4, len(flatten), 4):
+        initialPriority.append(flatten[i])   
+    return initialPriority  
+           
+            
+    
+   
+a = getID()
+  
+print(getID())
+print(getArrivaltime())
+print(getBursttime())
+print(getPriority())
+
+    
+                   
+        
+
+    
+    
+
+
+ 
---
+++ coen346-assign2-main/scheduler.py
@@ -0,0 +1,10 @@
+import time
+import readfile
+
+class Priority:
+    
+    def processData(self, no_of_processes):
+        process_data = []
+        no_of_processes = readfile.get_num_of_processes()
+        for i in range(int(read
+            
